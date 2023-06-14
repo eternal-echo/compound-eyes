614e1efbd6ed1164e22425973973bba843b0becc
yuanyu <yuanyu@whut.edu.cn>
2023-06-14 16:28:21 +0800

    客户端修改为多进程

--- video_client.py
+++ video_client.py
@@ -1,30 +1,32 @@
 import zmq
 import cv2
+import multiprocessing
 import time
 from datetime import datetime
 from wrappers import ImageWrapper
+import logging
 import json
 
-def main():
-    # 读取配置文件
-    with open('config.json', 'r') as f:
-        config = json.load(f)
+# 创建日志记录器
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+logger.addHandler(console_handler)
 
-    # 解析配置信息
-    camera_config = config['camera_config']
-    server_address = config['server_address']
-    server_port = config['server_port']
-
-    video = cv2.VideoCapture(0)
+# 图像采集进程
+def image_capture_worker(channel_id, camera_id, worker_url, context=None):
+    video = cv2.VideoCapture(camera_id)
 
     if not video.isOpened():
-        video.open(0)
+        video.open(camera_id)
 
-    context = zmq.Context()
+    context = context or zmq.Context.instance()
+
     socket = context.socket(zmq.PUB)
 
-    print("Collecting images from server...")
-    socket.connect("tcp://%s:%s" % (server_address, server_port))
+    print("Collecting images from server: {}".format(worker_url))
+    socket.connect(worker_url)
 
     read = True
     num_frames = 0
@@ -41,7 +43,7 @@ def main():
                 # initialize new protobuf image
                 image_wrapper = ImageWrapper()
                 # image_wrapper.copy_from_cv_image(image_gray)
-                image_wrapper.copy_from_cv_image(image, channel_id=1, timestamp=datetime.now().timestamp())
+                image_wrapper.copy_from_cv_image(image, channel_id=channel_id, timestamp=datetime.now().timestamp())
 
                 # serial to string, pack into message
                 msg = topic+image_wrapper.image_pb.SerializeToString()
@@ -55,6 +57,30 @@ def main():
 
     video.release()
     socket.close()
+
+def main():
+    multiprocessing.freeze_support()
+    # 读取配置文件
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    # 解析配置信息
+    camera_config = config['camera_config']
+    server_address = config['server_address']
+    server_port = config['server_port']
+
+    context = zmq.Context()
+    worker_url = "tcp://{}:{}".format(server_address, server_port)
+
+    # 创建图像读取进程
+    processes = []
+    for camera_info in camera_config:
+        channel_id = camera_info['channel_id']
+        camera_id = camera_info['camera_id']
+        p = multiprocessing.Process(target=image_capture_worker, args=(channel_id, camera_id, worker_url, context))
+        processes.append(p)
+        p.start()
+
     context.term()
 
 if __name__ == "__main__":
--- video_server.py
+++ video_server.py
@@ -1,21 +1,13 @@
 import zmq
 import cv2
+import threading
 from wrappers import ImageWrapper
 import json
 
-def main():
-    # 读取配置文件
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-
-    # 解析配置信息
-    camera_config = config['camera_config']
-    server_address = config['server_address']
-    server_port = config['server_port']
-
-    context = zmq.Context()
+def image_receive_worker(worker_url, context=None):
+    context = context or zmq.Context.instance()
     socket = context.socket(zmq.SUB)
-    socket.bind("tcp://*:{}".format(server_port))
+    socket.bind(worker_url)
 
     g_run = True
     num_frames = 0
@@ -38,7 +30,8 @@ def main():
                 # 显示时间戳
                 cv2.putText(image, str(image_wrapper.image_pb.timestamp), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                 cv2.putText(image, str(image_wrapper.image_pb.channel_id), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                cv2.imshow("display", image)
+                img_name = "channel_{}.jpg".format(image_wrapper.image_pb.channel_id)
+                cv2.imshow(img_name, image)
                 key = cv2.waitKey(1)
                 if key == 'q':
                     g_run = False
@@ -48,6 +41,22 @@ def main():
             g_run = False
 
     socket.close()
+
+def main():
+    # 读取配置文件
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    # 解析配置信息
+    camera_config = config['camera_config']
+    server_address = config['server_address']
+    server_port = config['server_port']
+
+    context = zmq.Context()
+    
+    worker_url = "tcp://*:{}".format(server_port)
+
+    image_receive_worker(worker_url, context=context)
     context.term()
 
 
